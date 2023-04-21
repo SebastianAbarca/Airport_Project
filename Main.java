8e6ba9374ee3f24ff36eb3a37732f3b6a867cb66
juanmazf <juanmazf16@gmail.com>
2023-04-21 16:51:07 -0400

    import bug fixed
    i fixed the passenger import problem by placing it in its own folder along the flight class so we can import the entire folder. also imported all the folder we have into main so we can use them

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import MyGraphs.*;
+import Tree.*;
+import Path.*;
 public class Main {
     public static void main(String[] args){
         
--- Tree/Node.java
+++ Tree/Node.java
@@ -1,6 +1,8 @@
 package Tree;
+import Objects.Passenger;
 
 public class Node {
-    
+    Node left,right;
+    Passenger person;
     
 }
